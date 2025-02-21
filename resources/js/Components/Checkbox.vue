c6a4a920b216178c25e6212984ebef09bb486e8a
Simon Moser <(simon.github@mailbox.org)>
2025-02-21 17:49:43 +0100

    initialize project structure with basic files and configurations
    Signed-off-by: Simon Moser <simon.github@mailbox.org>

---
+++ resources/js/Components/Checkbox.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:checked']);
+
+const props = defineProps<{
+    checked: boolean;
+    value?: any;
+}>();
+
+const proxyChecked = computed({
+    get() {
+        return props.checked;
+    },
+
+    set(val) {
+        emit('update:checked', val);
+    },
+});
+</script>
+
+<template>
+    <input
+        v-model="proxyChecked"
+        :value="value"
+        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+        type="checkbox"
+    />
+</template>
