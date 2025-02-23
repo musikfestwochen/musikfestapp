36fa107b80fe71b9fcefca38ced58ccd8ee3e070
Simon <simon.github@mailbox.org>
2025-02-23 21:12:54 +0100

    Add Larastan for static analysis and update user handling with null-safe operator
    Signed-off-by: Simon <simon.github@mailbox.org>

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -29,13 +29,13 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $request->user()?->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
+        if ($request->user()?->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $request->user()?->save();
 
         return Redirect::route('profile.edit');
     }
@@ -50,6 +50,10 @@ class ProfileController extends Controller
         ]);
 
         $user = $request->user();
+
+        if (!$user) {
+            abort(503);
+        }
 
         Auth::logout();
 
