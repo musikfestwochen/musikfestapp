c2b81e44d34e543a3efe0c7a4d0156a295b83532
smnmsr <40615828+smnmsr@users.noreply.github.com>
2025-03-10 18:16:50 +0000

    fix code style

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 covers(User::class);
 
 it('has correct fillable attributes', function () {
-    $user = new User();
+    $user = new User;
     expect($user->getFillable())->toEqualCanonicalizing([
         'name',
         'email',
@@ -15,12 +15,11 @@ it('has correct fillable attributes', function () {
 });
 
 it('has correct hidden attributes', function () {
-    $user = new User();
+    $user = new User;
     expect($user->getHidden())->toEqualCanonicalizing(['password', 'remember_token']);
 });
 
 it('implements MustVerifyEmail interface', function () {
-    $user = new User();
+    $user = new User;
     expect($user)->toBeInstanceOf(MustVerifyEmail::class);
 });
-
